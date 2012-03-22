36f4779d6cd2f47a6e5a7cf4cc287cefeb5713d4
Javier <nottu1990@gmail.com>
2012-03-22 15:18:46 -0600

    Agrege métodos para obtener información de las tablas
    Se regresa la información de las tablas en una lista de tuplas con el
    siguiente formato.
    
    [(Nombre, Tipo, Tamaño, Puede_Ser_Nulo),… ]

--- msq.py
+++ msq.py
@@ -19,14 +19,16 @@ def init():
 	entry.grid(column=3, row=1, columnspan=3,padx=10,pady=10)
 	
 	def getpsw(e):
-		global val
 		psw = entry.get()
 		if(psw):
 			data['password'] = psw
 			root.destroy()
-	entry.bind("<Return>",getpsw)
 	
+	entry.bind("<Return>",getpsw)
 	root.mainloop()
+	
+	getTables()
+	
 	return 'password' in data
 
 def ventanaPrincipal():
@@ -37,9 +39,13 @@ def ventanaPrincipal():
 	altasLb = ttk.Label(cntVP, text="Opciones:")
 	def alto():
 		darAltas(root)
+	def cons():
+		consultar(root)
+			
 	btnAlt = ttk.Button(cntVP, text = 'Altas',command = alto,width = 10)
 	btnBjs = ttk.Button(cntVP, text = 'Bajas',command = alto,width = 10)
 	btnRep = ttk.Button(cntVP, text = 'Reportes',command = alto,width = 10)
+	btnCons = ttk.Button(cntVP, text = 'Consultas',command = alto,width = 10)
 	
 	cntVP.grid(	column=0,	row=0)
 	altasLb.grid(	column=0,	row=1, columnspan=2,padx=10)
@@ -61,11 +67,30 @@ def getTables():
 		cr.execute(query)
 		for val in cr:
 			tables.append(val[0].title())
+		db.close()
 	except ValueError:
 		print 'err'
 
+def consultar(papa):
+	pass
+	
+def getTblInfo(tbl):
+	try:
+		db = mysql.connect(**data)
+		cr = db.cursor()
+		query = """
+		SELECT COLUMN_NAME,  DATA_TYPE, CHARACTER_MAXIMUM_LENGTH , IS_NULLABLE
+		from INFORMATION_SCHEMA.COLUMNS 
+		where table_name=""" +tbl +"and table_schema = 'escuela'"
+		cr.execute(query)
+		res = [n for n in cr]
+		db.close()
+		return res
+	except ValueError:
+		pass
+
 def darAltas(papa):
-	tablaA = ''
+	tablaA = None
 	print 'dar alta'
 	vent = Toplevel(papa)
 	vent.title('Altas')
@@ -75,30 +100,22 @@ def darAltas(papa):
 	tbls	= ttk.Combobox(cntAl,width = 15)
 	tblsLb	= ttk.Label(cntAl, text="Tablas:")
 	tbls['values'] = tables
-	tblsLb.grid(column=0, row=0,padx=5)
-	tbls.grid(column=1, row=0,padx=5)
+	
 	def obtener(e):
 		global tablaA
 		tablaA = tbls.get()
 	tbls.bind('<<ComboboxSelected>>', obtener)
+	
+	tblsLb.grid(	column=0, row=0,padx=5)
+	tbls.grid(		column=1, row=0,padx=5)
+
 
 if __name__ == '__main__':
 	#if(not init()): exit() #si no inicializa bien, salir
+	
+	#inicialización manual
 	data['password'] = '!Snowbasin1)'
 	getTables()
+	#
+
 	ventanaPrincipal()
-	#print tables
-	#ventanaPrincipal()
-	"""
-	try:
-		db = mysql.connect(**data)
-		cr = db.cursor()
-		query = 'select * from Alumnos'
-		cr.execute(query)
-		for row in cr: 
-			for n in row:
-				print str(n) +" ",
-			print ""
-	except ValueError:
-		print 'err'
-	"""
